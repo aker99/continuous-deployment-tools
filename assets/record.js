e5c5d907ec030f2e30574bb94c5370437e1eba2c
Himanshu Kotnala <himanshu.kotnala99@gmail.com>
2020-05-10 01:25:10 +0530

    Create record.js

---
+++ assets/record.js
@@ -0,0 +1,7 @@
+{
+    "<username/reponame>" : {
+        "branch" : "<branch to be deployed>",
+        "dir" : "<directory to be deployed>",
+        "script" : "<script from ./script folder to excuted for deployment>"
+    }
+}
