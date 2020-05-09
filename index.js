b0c7e19153b84b707af80acd0fd81b5ce0d4e02c
Himanshu Kotnaa <himanshu.kotnaa99@gmail.com>
2020-05-09 15:12:16 +0000

    Test: Body Parser

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
 const port  = 8050;
 
 const cors = require('cors')
 
 
-app.use(cors())
+app.use(cors());
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 
 app.get('', (req,res) => {
     res.send('Hello, It\'s a deployment module');
 });
 
 app.post('/aker99/frontend', (req,res) => {
-    console.log('req:',req);
+    console.log('req:',req.body);
     res.send('Done');
 });
 
